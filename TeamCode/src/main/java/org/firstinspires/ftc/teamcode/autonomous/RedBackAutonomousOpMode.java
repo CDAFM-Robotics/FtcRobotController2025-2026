b7fb826f43c8d80743548e8e0a246b9585934643
jonat <jonathanzwitt@gmail.com>
2025-09-27 14:52:57 -0500

    Autonomous stuffs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedBackAutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedBackAutonomousOpMode.java
@@ -1,10 +1,33 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.autonomous.trajectories.AutonomousTrajectoryBuilder;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+@Autonomous(name = "Red Back Autonomous")
 public class RedBackAutonomousOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        MecanumDrive md = new MecanumDrive(hardwareMap, new Pose2d(new Vector2d(61, 11.5), Math.toRadians(180)));
+        AutonomousTrajectoryBuilder autonomousTrajectoryBuilder = new AutonomousTrajectoryBuilder(md);
+
+        Action[] trajectories = autonomousTrajectoryBuilder.getRedFarTrajectories();
+
+        waitForStart();
+
+        Actions.runBlocking(trajectories[0]);
+        sleep(2000);
+        Actions.runBlocking(trajectories[1]);
+        sleep(2000);
+        Actions.runBlocking(trajectories[2]);
+        sleep(2000);
+        Actions.runBlocking(trajectories[3]);
 
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/trajectories/AutonomousTrajectoryBuilder.java
@@ -0,0 +1,146 @@
+package org.firstinspires.ftc.teamcode.autonomous.trajectories;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.VelConstraint;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+public class AutonomousTrajectoryBuilder {
+
+    public Action redFarStartToFarLaunch;
+    public Action redFarLaunchPickupThirdMark;
+    public Action redFarLaunchPickupHumanPlayerZone;
+    public Action redFarLaunchToLeaveLaunchZone;
+
+    public Action redCloseStartToCloseLaunch;
+    public Action redCloseLaunchPickupFirstMark;
+    public Action redCloseLaunchPickupSecondMark;
+    public Action redCloseLaunchToLeaveLaunchZone;
+
+    public Action blueFarStartToFarLaunch;
+    public Action blueFarLaunchPickupThirdMark;
+    public Action blueFarLaunchPickupHumanPlayerZone;
+    public Action blueFarLaunchToLeaveLaunchZone;
+
+    public Action blueCloseStartToCloseLaunch;
+    public Action blueCloseLaunchPickupFirstMark;
+    public Action blueCloseLaunchPickupSecondMark;
+    public Action blueCloseLaunchToLeaveLaunchZone;
+
+    public Pose2d redFarLaunchPose = new Pose2d(57.7, 12.5, Math.toRadians(165));
+    public Pose2d redCloseLaunchPose = new Pose2d(new Vector2d(-20, 20), Math.toRadians(135));
+
+    public Pose2d blueFarLaunchPose = new Pose2d(57.7, -12.5, Math.toRadians(165));
+    public Pose2d blueCloseLaunchPose = new Pose2d(new Vector2d(-20, -20), Math.toRadians(-135));
+
+    public VelConstraint normalTranslationalVelConstraint = new TranslationalVelConstraint(20);
+    public VelConstraint slowTranslationalVelConstraint = new TranslationalVelConstraint(10);
+
+    public AutonomousTrajectoryBuilder(MecanumDrive md) {
+
+        redFarStartToFarLaunch = md.actionBuilder(new Pose2d(new Vector2d(61, 11.5), Math.toRadians(180)))
+            .strafeToLinearHeading(redFarLaunchPose.position, redFarLaunchPose.heading, normalTranslationalVelConstraint)
+            .build();
+
+        redFarLaunchPickupThirdMark = md.actionBuilder(redFarLaunchPose)
+          .setTangent(Math.toRadians(165))
+          .splineToSplineHeading(new Pose2d(36, 23.5, Math.toRadians(90)), Math.toRadians(90), normalTranslationalVelConstraint)
+          .strafeToConstantHeading(new Vector2d(36, 53), slowTranslationalVelConstraint)
+          .setTangent(Math.toRadians(-90))
+          .splineToSplineHeading(redFarLaunchPose, Math.toRadians(-15), normalTranslationalVelConstraint)
+          .build();
+
+        redFarLaunchPickupHumanPlayerZone = md.actionBuilder(redFarLaunchPose)
+            .setTangent(Math.toRadians(90))
+            .splineToSplineHeading(new Pose2d(60, 47.5, Math.toRadians(90)), Math.toRadians(90), normalTranslationalVelConstraint)
+            .strafeToConstantHeading(new Vector2d(60, 61), slowTranslationalVelConstraint)
+            .setTangent(Math.toRadians(-90))
+            .splineToSplineHeading(redFarLaunchPose, Math.toRadians(-90), normalTranslationalVelConstraint)
+            .build();
+
+        redFarLaunchToLeaveLaunchZone = md.actionBuilder(redFarLaunchPose)
+          .lineToX(43, normalTranslationalVelConstraint)
+          .build();
+
+        redCloseStartToCloseLaunch = md.actionBuilder(new Pose2d(-50.5, 50.5, Math.toRadians(-53)))
+          .strafeToSplineHeading(redCloseLaunchPose.position, redCloseLaunchPose.heading, normalTranslationalVelConstraint)
+          .build();
+
+        redCloseLaunchPickupFirstMark = md.actionBuilder(redCloseLaunchPose)
+            .setTangent(Math.toRadians(0))
+            .splineToSplineHeading(new Pose2d(new Vector2d(-11.5, 26), Math.toRadians(90)), Math.toRadians(90), normalTranslationalVelConstraint)
+            .strafeToConstantHeading(new Vector2d(-11.5, 46), slowTranslationalVelConstraint)
+            .setTangent(Math.toRadians(-90))
+            .splineToSplineHeading(redCloseLaunchPose, Math.toRadians(-135), normalTranslationalVelConstraint)
+            .build();
+
+        redCloseLaunchPickupSecondMark = md.actionBuilder(redCloseLaunchPose)
+            .setTangent(Math.toRadians(10))
+            .splineToSplineHeading(new Pose2d(new Vector2d(11.5, 30), Math.toRadians(90)), Math.toRadians(90), normalTranslationalVelConstraint)
+            .strafeToConstantHeading(new Vector2d(11.5, 46), slowTranslationalVelConstraint)
+            .setTangent(Math.toRadians(-135))
+            .splineToSplineHeading(redCloseLaunchPose, Math.toRadians(-135), normalTranslationalVelConstraint)
+            .build();
+
+        redCloseLaunchToLeaveLaunchZone = md.actionBuilder(redCloseLaunchPose)
+            .strafeToConstantHeading(new Vector2d( -10, 30))
+            .build();
+
+        blueFarStartToFarLaunch = md.actionBuilder(new Pose2d(new Vector2d(61, -11.5), Math.toRadians(180)))
+            .strafeToLinearHeading(blueFarLaunchPose.position, blueFarLaunchPose.heading, normalTranslationalVelConstraint)
+            .build();
+
+        blueFarLaunchPickupThirdMark = md.actionBuilder(blueFarLaunchPose)
+            .setTangent(Math.toRadians(-165))
+            .splineToSplineHeading(new Pose2d(36, -23.5, Math.toRadians(-90)), Math.toRadians(-90), normalTranslationalVelConstraint)
+            .strafeToConstantHeading(new Vector2d(36, -53), slowTranslationalVelConstraint)
+            .setTangent(Math.toRadians(90))
+            .splineToSplineHeading(blueFarLaunchPose, Math.toRadians(15), normalTranslationalVelConstraint)
+            .build();
+
+        blueFarLaunchPickupHumanPlayerZone = md.actionBuilder(blueFarLaunchPose)
+            .setTangent(Math.toRadians(-90))
+            .splineToSplineHeading(new Pose2d(60, -47.5, Math.toRadians(-90)), Math.toRadians(-90), normalTranslationalVelConstraint)
+            .strafeToConstantHeading(new Vector2d(60, -61), slowTranslationalVelConstraint)
+            .setTangent(Math.toRadians(90))
+            .splineToSplineHeading(blueFarLaunchPose, Math.toRadians(90), normalTranslationalVelConstraint)
+            .build();
+
+        blueFarLaunchToLeaveLaunchZone = md.actionBuilder(blueFarLaunchPose)
+            .lineToX(43, normalTranslationalVelConstraint)
+            .build();
+
+        blueCloseStartToCloseLaunch = md.actionBuilder(new Pose2d(-50.5, -50.5, Math.toRadians(53)))
+            .strafeToSplineHeading(blueCloseLaunchPose.position, blueCloseLaunchPose.heading, normalTranslationalVelConstraint)
+            .build();
+
+        // TODO: 9/27/2025 Reverse the trajectories' y values, angles, and tangents down from this point.
+
+        blueCloseLaunchPickupFirstMark = md.actionBuilder(blueCloseLaunchPose)
+            .setTangent(Math.toRadians(0))
+            .splineToSplineHeading(new Pose2d(new Vector2d(-11.5, 26), Math.toRadians(90)), Math.toRadians(90), normalTranslationalVelConstraint)
+            .strafeToConstantHeading(new Vector2d(-11.5, 46), slowTranslationalVelConstraint)
+            .setTangent(Math.toRadians(-90))
+            .splineToSplineHeading(blueCloseLaunchPose, Math.toRadians(-135), normalTranslationalVelConstraint)
+            .build();
+
+        blueCloseLaunchPickupSecondMark = md.actionBuilder(blueCloseLaunchPose)
+            .setTangent(Math.toRadians(10))
+            .splineToSplineHeading(new Pose2d(new Vector2d(11.5, 30), Math.toRadians(90)), Math.toRadians(90), normalTranslationalVelConstraint)
+            .strafeToConstantHeading(new Vector2d(11.5, 46), slowTranslationalVelConstraint)
+            .setTangent(Math.toRadians(-135))
+            .splineToSplineHeading(blueCloseLaunchPose, Math.toRadians(-135), normalTranslationalVelConstraint)
+            .build();
+
+        blueCloseLaunchToLeaveLaunchZone = md.actionBuilder(blueCloseLaunchPose)
+            .strafeToConstantHeading(new Vector2d( -10, 30))
+            .build();
+    }
+
+    public Action[] getRedFarTrajectories() {
+        return new Action[] {redFarStartToFarLaunch, redFarLaunchPickupThirdMark, redFarLaunchPickupHumanPlayerZone, redFarLaunchToLeaveLaunchZone};
+    }
+}
