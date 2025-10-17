1f112ea9be91dc6db964fc61db0f6ad338ff2c24
Jeremy Witt <jeremychandlerwitt@gmail.com>
2025-10-17 14:49:36 -0500

    Better but not yet working Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Auto_RR_MotorSetupTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Auto_RR_MotorSetupTest.java
@@ -1,15 +1,20 @@
 package org.firstinspires.ftc.teamcode.testing;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.autonomous.actions.AutonomousActionBuilder;
 import org.firstinspires.ftc.teamcode.common.Robot;
+import org.firstinspires.ftc.teamcode.roadrunner.Drawing;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
 @Autonomous(name = "Auto_RR_MotorSetupTest", group = "Testing")
@@ -18,43 +23,64 @@ public class Auto_RR_MotorSetupTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         // Start at Center of Field (Red Alliance ref) bot facing forwards (along positive Y axis = 90 degrees(PI/2) on unit circle)
-        MecanumDrive md = new MecanumDrive(hardwareMap, new Pose2d(0, 0, Math.toRadians(90)));
-        Robot robot = new Robot(hardwareMap, telemetry);
 
-        AutonomousActionBuilder autonomousTrajectoryBuilder = new AutonomousActionBuilder(md, robot);
+        // TODO : SET ROBOT UP FIRST SINCE IT ESTABLISHES SOME VARIABLES
+        // Robot robot = new Robot(hardwareMap, telemetry);
 
-        Action[] trajectories = autonomousTrajectoryBuilder.getRedFarTrajectories();
+        MecanumDrive md = new MecanumDrive(hardwareMap, new Pose2d(0, 0, Math.toRadians(0)));
 
-        Action[] otherActions = autonomousTrajectoryBuilder.getOtherActions();
 
+//        AutonomousActionBuilder autonomousTrajectoryBuilder = new AutonomousActionBuilder(md, robot);
+//
+//        Action[] trajectories = autonomousTrajectoryBuilder.getRedFarTrajectories();
+//
+//        Action[] otherActions = autonomousTrajectoryBuilder.getOtherActions();
+//
         Robot.ArtifactColor[] motif = null;
 
         while(opModeInInit()) {
-            motif = robot.getLauncher().getMotifPattern();
+//            motif = robot.getLauncher().getMotifPattern();
         }
 
         waitForStart();
 
-        //start always with PGP
-
-        if (motif == null) {
-            motif = new Robot.ArtifactColor[] {Robot.ArtifactColor.GREEN, Robot.ArtifactColor.PURPLE, Robot.ArtifactColor.PURPLE};
-        }
-
-        if (motif[0] != Robot.ArtifactColor.GREEN) {
-            Actions.runBlocking(otherActions[2]);
-        }
+//        //start always with PGP
+//
+//        if (motif == null) {
+//            motif = new Robot.ArtifactColor[] {Robot.ArtifactColor.GREEN, Robot.ArtifactColor.PURPLE, Robot.ArtifactColor.PURPLE};
+//        }
+//
+//        if (motif[0] != Robot.ArtifactColor.GREEN) {
+//            Actions.runBlocking(otherActions[2]);
+//        }
 
         // Go to the Launch Pose
 
         Actions.runBlocking(new ParallelAction(
-            // trajectories[0] //
+            // trajectories[0]
             // otherActions[0]  // TODO SPINUPASCTION
                 // TODO basic start and move forward.
-                md.actionBuilder(new Pose2d(new Vector2d(0,0), Math.toRadians(90)))
-                        .strafeTo(new Vector2d(0, 24))  // Move forward one square
+                md.actionBuilder(new Pose2d(new Vector2d(0,0), Math.toRadians(0)))
+                        .strafeTo(new Vector2d(0, 1))  // Move forward one square
+                        .turnTo(Math.PI/2)
+                        .strafeTo(new Vector2d(0,0))
                 .build()
         ));
+
+        // TODO Jer Added for
+        md.updatePoseEstimate();
+
+        Pose2d pose = md.localizer.getPose();
+        telemetry.addData("x", pose.position.x);
+        telemetry.addData("y", pose.position.y);
+        telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
+        telemetry.update();
+
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.fieldOverlay().setStroke("#3F51B5");
+        Drawing.drawRobot(packet.fieldOverlay(), pose);
+        FtcDashboard.getInstance().sendTelemetryPacket(packet);
+
 /*
         Actions.runBlocking(otherActions[5]);
 
