31a746d9d38af5401beb916771e5037f8399048e
Jonathan Witt <jonathanzwitt@gmail.com>
2026-02-12 17:40:02 -0600

    Pedro Pathing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Constants.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.pedropathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+        .mass(11.33981);
+
+    public static MecanumConstants mecanumConstants = new MecanumConstants()
+        .maxPower(1)
+        .rightFrontMotorName("frontRightMotor")
+        .rightRearMotorName("backRightMotor")
+        .leftFrontMotorName("frontLeftMotor")
+        .leftRearMotorName("backLeftMotor")
+        .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+        .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+        .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+        .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static PinpointConstants pinpointConstants = new PinpointConstants()
+        .forwardPodY(8)
+        .strafePodX(-3.25)
+        .distanceUnit(DistanceUnit.INCH)
+        .hardwareMapName("pinpoint")
+        .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+        .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+        .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+            .pinpointLocalizer(pinpointConstants)
+            .pathConstraints(pathConstraints)
+            .mecanumDrivetrain(mecanumConstants)
+            .build();
+    }
+}
